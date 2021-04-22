4d3b937905d56b8edc12970c20a5be874afd3c0c
MatteoLupica <61271439+MatteoLupica@users.noreply.github.com>
2021-04-22 14:49:54 +0200

    Modifica codice sorgente server

--- Scanner_Lidar/Server/Models/Server.cs
+++ Scanner_Lidar/Server/Models/Server.cs
@@ -23,17 +23,44 @@ namespace Server_Lidar.Models
         #endregion
 
         #region =================== membri statici =============
+        /// <summary>
+        /// Indica il logger utilizzato per avere traccia di quello che accade.
+        /// </summary>
         private static Logger myLogger = LogManager.GetCurrentClassLogger();
+        /// <summary>
+        /// Indica se il server è abilitato a mandare i dati ai client.
+        /// </summary>
         private static bool canSend = true;
+        /// <summary>
+        /// Indica se il server sta mandando i dati.
+        /// </summary>
         private static bool isSending = false;
+        /// <summary>
+        /// Indica il socket utilizzato per effetuare il binding della porta da parte del server.
+        /// </summary>
         private static TcpListener tcpListener;
+        /// <summary>
+        /// Indica la lista dei punti che vengono scansionati
+        /// </summary>
         private static List<Vector3> vector3s = new List<Vector3>();
-        private static CancellationTokenSource ts;
-        private static CancellationToken end;
+        /// <summary>
+        /// Indica la sorgente del token.
+        /// </summary>
+        private static CancellationTokenSource ts = new CancellationTokenSource();
+        /// <summary>
+        /// Indica il token per poter cancellare una determinata task.
+        /// </summary>
+        private static CancellationToken end = ts.Token;
         #endregion
 
         #region =================== membri & proprietà =========
+        /// <summary>
+        /// Indica la porta seriale utilizzata dal computer per comunicare con arduino.
+        /// </summary>
         private SerialPort arduino;
+        /// <summary>
+        /// Indica la porta del server.
+        /// </summary>
         private int serverPort;
         
         public int ServerPort
@@ -58,18 +85,16 @@ namespace Server_Lidar.Models
         #endregion
 
         #region =================== costruttori ================
-        public Server(int port)
-        {
-            ServerPort = port;
-            ts = new CancellationTokenSource();
-            end = ts.Token;
-        }
-        public Server(int serverPort,string serialPort,int baudRate, int max_points)
+        /// <summary>
+        /// Costruttore per inizializzare il server.
+        /// </summary>
+        /// <param name="serverPort">Indica la porta del server</param>
+        /// <param name="serialPort">Indica la porta seriale (COM)</param>
+        /// <param name="baudRate">Indica la velocità dei dati in bit al secondo.</param>
+        public Server(int serverPort,string serialPort,int baudRate)
         {
             ServerPort = ServerPort;
             arduino = new SerialPort(serialPort, baudRate);
-            ts = new CancellationTokenSource();
-            end = ts.Token;
         }
         #endregion
 
@@ -107,6 +132,7 @@ namespace Server_Lidar.Models
             int cnt = 0;
             if (arduino != null)
             {
+                // Avvio un'attività in parallelo al processo principale
                 Task.Factory.StartNew(() =>
                 {
                     try
@@ -148,7 +174,10 @@ namespace Server_Lidar.Models
                                         {
                                             arduino.Write("OK");
                                             myLogger.Fatal("RESET DATA REQUEST TO ARDUINO");
-                                        }else if (reader.Contains("<EOF>"))
+                                            vector3s.Clear();
+                                            cnt = 0;
+                                        }
+                                        else if (reader.Contains("<EOF>"))
                                         {
                                             myLogger.Info("Arduino send all data");
                                             ts.Cancel();
@@ -397,19 +426,12 @@ namespace Server_Lidar.Models
                 else if (header.Contains("GET / HTTP/1.1"))
                 {
                     TcpClient web = client;
-                    string path = Path.Combine(AppContext.BaseDirectory, @"..\..\..\Request\web.html");
-                    string page = File.ReadAllText(path);
-                    var utf8 = new UTF8Encoding();
-                    web.GetStream().Write(utf8.GetBytes("HTTP/1.1 \r\n 200 OK"));
-                    web.GetStream().Write(utf8.GetBytes("ContentType: text/html\r\n"));
-                    web.GetStream().Write(utf8.GetBytes("\r\n"));
-                    web.GetStream().Write(utf8.GetBytes(page));
-                    web.GetStream().Write(utf8.GetBytes("\r\n\r\n"));
-                    web.GetStream().Flush();
+                    web.Close();
+                    return;
                 }
                 else
                 {
-                    myLogger.Warn("No header element passed");
+                    myLogger.Warn("No protocol supported");
                 }
             }
             myLogger.Warn(String.Format("The client {0} disconnected", client.Client.RemoteEndPoint));
--- Scanner_Lidar/Server/Program.cs
+++ Scanner_Lidar/Server/Program.cs
@@ -2,99 +2,110 @@
 using Server_Lidar.Models;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO.Ports;
 using System.Linq;
+using System.Net;
 using System.Threading;
 
 namespace Server_Lidar
 {
     class Program
     {
-        private const int SERVER_PORT = 12345;
+        /// <summary>
+        /// Indica il logger utilizzato per avere traccia di quello che accade.
+        /// </summary>
         private static Logger myLogger = LogManager.GetCurrentClassLogger();
-        private const int BAUD_RATE = 115200;
-        private const int MAX_DATA_SCANNED = 900;
+        /// <summary>
+        /// Indica la porta del server.
+        /// </summary>
+        private static int serverPort;
+        /// <summary>
+        /// Indica la velocità dei dati in bit al secondo.
+        /// </summary>
+        private static int baudRate;
+        /// <summary>
+        /// Indica la porta seriale utilizzata per comunicare con arduino.
+        /// </summary>
+        private static string comPort;
         static void Main(string[] args)
         {
-            Console.Title = "Server_Lidar";
+            Init();
+            Console.Title = String.Format("Server_Lidar {0}:{1}", Dns.GetHostEntry(Dns.GetHostName()).HostName,serverPort);
+            comPort = DetectArduino();
             Server server = null;
-            string comPort = DetectArduino();
             if(!comPort.Equals("NO PORT FOUND"))
             {
-                server = new Server(SERVER_PORT, comPort, BAUD_RATE, MAX_DATA_SCANNED);
-            }
-            else
-            {
-                server = new Server(SERVER_PORT);
-            }
-            try
-            {
-                server.Start();
-            }
-            catch (Exception e)
-            {
-                myLogger.Error(e.Message);
-            }
-            finally
-            {
-                server.Stop();
+                server = new Server(serverPort, comPort, baudRate);
+                try
+                {
+                    server.Start();
+                }
+                catch (Exception e)
+                {
+                    myLogger.Error(e.Message);
+                }
+                finally
+                {
+                    server.Stop();
+                }
             }
             Console.WriteLine("Premi un pulsante per finire...");
             Console.ReadLine();
         }
         /// <summary>
+        /// Inizializza le componenti per l'avvio del server
+        /// </summary>
+        private static void Init()
+        {
+            baudRate = int.Parse(ConfigurationManager.AppSettings.Get("BaudRate"));
+            serverPort = int.Parse(ConfigurationManager.AppSettings.Get("ServerPort"));
+        }
+
+        /// <summary>
         /// Il metodo si connette alle porte COM disponibili fino a cercare quello che è collegata ad arduino.
         /// Se sulla porta a cui si connette è collegato arduino, l'arduino comunica al pc di essere arduino e quindi di usare quella porta.
         /// </summary>
         /// <returns>La porta COM dove è collegato l'arduino</returns>
+        /// <copyright>Matteo Lupica</copyright>
         public static string DetectArduino()
         {
+            bool found = false;
             string[] availblePort = SerialPort.GetPortNames();
             List<string> ports = availblePort.OfType<string>().ToList();
             List<string> disponibili = new List<string>();
             SerialPort serial = new SerialPort();
-            serial.BaudRate = BAUD_RATE;
+            serial.BaudRate = baudRate;
             int cnt = 0;
-        StartLoop:
-            try
+            while (true && !found)
             {
-                serial.PortName = ports[cnt < ports.Count - 1 ? cnt++ : cnt];
-                myLogger.Debug("Porta attuale: " + serial.PortName);
-                serial.Open();
-                while (true)
+                try
                 {
-                    try
+                    serial.PortName = ports[cnt < ports.Count - 1 ? cnt++ : cnt];
+                    myLogger.Debug("Porta attuale: " + serial.PortName);
+                    serial.Open();
+                    Thread.Sleep(500);
+                    if (serial.BytesToRead > 0)
                     {
-                        Thread.Sleep(500);
-                        if (serial.BytesToRead > 0)
+                        string reader = serial.ReadExisting();
+                        if (reader.Contains("CIAO"))
                         {
-                            string reader = serial.ReadExisting();
-                            if (reader.Contains("CIAO"))
-                            {
-                                disponibili.Add(serial.PortName);
-                                myLogger.Debug("SONO ARDUINO");
-                                goto EndLoop;
-                            }
+                            disponibili.Add(serial.PortName);
+                            myLogger.Debug("SONO ARDUINO");
+                            found = true;
                         }
-                        else
-                        {
-                            serial.Close();
-                            goto StartLoop;
-                        }
+                    }
+                    else
+                    {
+                        serial.Close();
+                    }
 
-                    }
-                    catch (InvalidOperationException ioe)
-                    {
-                        myLogger.Error(ioe.Message);
-                    }
+                }
+                catch (Exception ioe)
+                {
+                    myLogger.Error(ioe.Message);
                 }
             }
-            catch (Exception e)
-            {
-                myLogger.Error(e.Message);
-                goto StartLoop;
-            }
-        EndLoop:
             serial.Write("OK");
             serial.Close();
             foreach (var item in disponibili)
@@ -105,4 +116,3 @@ namespace Server_Lidar
         }
     }
 }
-
